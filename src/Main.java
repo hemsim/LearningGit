74a639403948543e2dbafcc51cb125f74f4fd24a
hemsim <hemzalovasimona@gmail.com>
2024-03-07 01:38:02 +0100

    changes

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import window.Window;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -13,5 +15,8 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
+
+        Window window = new Window();
+        System.out.println(window);
     }
 }
--- src/window/Window.java
+++ src/window/Window.java
@@ -1,2 +1,10 @@
-package window;public class Window {
+package window;
+
+public class Window {
+    private String window;
+
+    @Override
+    public String toString() {
+        return "this is a window";
+    }
 }
