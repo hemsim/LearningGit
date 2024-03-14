b89ed2bd6239767f9beebce19721d9c634081df6
hemsim <118842582+hemsim@users.noreply.github.com>
2024-03-14 01:22:27 +0100

    added text "test"

---
+++ src/window/Test.java
@@ -0,0 +1,12 @@
+package window;
+
+public class Test {
+    String text = "test";
+
+    @Override
+    public String toString() {
+        return "Test{" +
+                "text='" + text + '\'' +
+                '}';
+    }
+}
